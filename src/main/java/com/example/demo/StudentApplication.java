e9254756dd2e4532724426b8ef53e34ff1ba8600
bilelfeki <bilel.feki.92@gmail.com>
2022-03-10 22:04:32 +0100

    generate get with @Controler

--- src/main/java/com/example/demo/StudentApplication.java
+++ src/main/java/com/example/demo/StudentApplication.java
@@ -2,12 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Controller
+@RequestMapping
 public class StudentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApplication.class, args);
+
+	}
+	@GetMapping
+	public @ResponseBody
+	String genereteGet(){
+		return "hello" ;
 	}
 
 }
